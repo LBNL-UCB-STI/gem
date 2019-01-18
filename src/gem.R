a4c69b2d95f0ad8d85f83dfb415bd0d2222cec07
Colin Sheppard <colinthegaunt@gmail.com>
2019-01-18 12:44:58 -0800

    running exper

--- src/gem.R
+++ src/gem.R
@@ -34,7 +34,7 @@ source('input/defaults.R')
 #####################################################################################
 option_list <- list()
 if(interactive()){
-  args<-'input/experiments/base.yaml'
+  args<-'input/experiments/test.yaml'
   args <- parse_args(OptionParser(option_list = option_list,usage = "gem.R [exp-file]"),positional_arguments=T,args=args)
 }else{
   args <- parse_args(OptionParser(option_list = option_list,usage = "gem.R [exp-file]"),positional_arguments=T)
