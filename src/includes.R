2fc1dca8571dba475d0e29a437f38bee5c429abf
Alan Jenn <headisbagent@gmail.com>
2019-11-24 21:03:26 -0800

    Includes sync

--- src/includes.R
+++ src/includes.R
@@ -12,6 +12,9 @@ library('RColorBrewer')
 library('forcats')
 library('gtools')
 library('sf')
+library('tidyr')
+library('dplyr')
+library('cowplot')
 
 gdx.to.data.tables <- function(mygdx){
   keys <- all_items(mygdx)$set
