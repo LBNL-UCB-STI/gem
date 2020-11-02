937cdc8cc5093dffd76496063613ad05bbc12fdb
WanshiHong <wanshihong@lbl.gov>
2020-11-02 15:11:14 -0800

    plot

--- src/gem.R
+++ src/gem.R
@@ -136,7 +136,37 @@ if(!args$plots){ # only prep and run model if *not* in plot-only mode
 #####################################################################################
 # Post-Process Results
 #####################################################################################
-plots.dir <- pp(exper$input.dir,'/plots/')
+plots.dir1 <- pp(exper$input.dir,'/plots1/')
+make.dir(plots.dir1)
+write.csv(exper$runs,pp(plots.dir1,'runs.csv'),row.names=T)
+results <- list(); i<-1
+for(i in 1:nrow(exper$runs)) {
+  result <- gdx.to.data.tables(gdx(pp(exper$input.dir,'/runs/run-',i,'/results.gdx')))
+  result.baseGen <- gdx.to.data.tables(gdx(pp(exper$input.dir,'/runs/run-',i,'/results-baseGeneration.gdx')))
+  result <- merge.baseGen(result,result.baseGen)
+  for(key in names(result)){
+    result[[key]][,run:=i]
+    if('t'%in%names(result[[key]]) & args$trimdays){
+      max.t <- max(result[[key]]$t)
+      result[[key]] <- result[[key]][t>24 & t<=max.t-24]
+      result[[key]][,t:=t-24]
+    }
+    if(i==1)results[[key]] <- list()
+    results[[key]][[length(results[[key]])+1]] <- result[[key]]
+  }
+  if(args$trimdays){
+    all.inputs[[i]]$sets$t <- all.inputs[[i]]$set$t[1:(length(all.inputs[[i]]$set$t)-48)]
+  }
+  make.dir(pp(plots.dir1,'/run-',i,''))
+}
+res <- lapply(results,function(ll){ rbindlist(ll,fill=T) })
+
+plots.mobility(exper,all.inputs,res,plots.dir1)
+
+
+
+#truck plot
+plots.dir <- pp(exper$input.dir,'/plotstruck/')
 make.dir(plots.dir)
 write.csv(exper$runs,pp(plots.dir,'runs.csv'),row.names=T)
 results <- list(); i<-1
